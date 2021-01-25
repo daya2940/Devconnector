cac6893da78936224ef55c3845660f881063a26a
saurav kumar <sauuit2940@gmail.com>
2021-01-25 18:42:38 +0530

    Code refractored

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
 
   //check token
   if (token) {
-    return res.status(401).json({ msg: "No token authorization denied" });
+    return res.status(401).json({ msg: "No token ,authorization denied" });
   }
 
   //verify token
